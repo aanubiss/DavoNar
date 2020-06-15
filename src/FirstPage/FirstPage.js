ba2cf0863f9edaae47903379a7db09b989d17f17
aanubiss <44551421+aanubiss@users.noreply.github.com>
2020-06-15 16:57:48 +0400

    qwr

--- src/FirstPage/FirstPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default FirstPage = () => {
-    return (
-        <div style={styles.esim}>
-            
-        </div>
-    )
-}
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default Header = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
